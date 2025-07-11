d4adcb3fea59cd776a3d8f94a01a1a889307a142
Conno <Conno@CONNOR_LENOVO>
2025-07-10 23:47:02 -0500

    First assignment done moving to linux

--- javascript.js
+++ javascript.js
@@ -1,11 +1,20 @@
-let name = "John";
-let surname = "Doe";
+console.log(23 + 97);
 
-console.log(name);
-console.log(surname);
+console.log(6+7+88+96+6+3);
 
-let age = 11;
-console.log(age);
+console.log((4+6+9)/77);
 
-age = 54;
-console.log(age);
+let a = 10;
+console.log(a);
+a = 8;
+console.log(a);
+
+let b = 7*a;
+console.log(b);
+
+const max = 57;
+const actual  = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage);
